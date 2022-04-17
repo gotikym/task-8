b3082ec52b5944f419cf4eeeafda58cddc0c6556
Ivan Malkovskii <103892616+gotikym@users.noreply.github.com>
2022-04-17 23:31:35 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -4,12 +4,12 @@ internal class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Сколько бабуль перед тобой?");
-        var grandmothers = Convert.ToByte(Console.ReadLine());
-        var timeReceipt = 10;
-        var hour = 60;
-        var timeWaiting = grandmothers * timeReceipt;
-        var timeHour = timeWaiting / hour;
-        var timeMinuts = timeWaiting % hour;
+        byte grandmothers = Convert.ToByte(Console.ReadLine());
+        const byte timeReceipt = 10;
+        const byte hour = 60;
+        ushort timeWaiting = grandmothers * timeReceipt;
+        ushort timeHour = timeWaiting / hour;
+        byte timeMinuts = timeWaiting % hour;
         Console.WriteLine($"Если не будет всяких (я только спросить), то ждать осталось {timeHour} часов и {timeMinuts} минут");
     }
 }
