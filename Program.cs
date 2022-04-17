f565919c4bf3d00b392c2e7c392f86f6846e8bf2
One <One@DESKTOP-MIB22JC>
2022-04-17 23:16:44 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Сколько бабуль перед тобой?");
+        var grandmothers = Convert.ToByte(Console.ReadLine());
+        var timeReceipt = 10;
+        var hour = 60;
+        var timeWaiting = grandmothers * timeReceipt;
+        var timeHour = timeWaiting / hour;
+        var timeMinuts = timeWaiting % hour;
+        Console.WriteLine($"Если не будет всяких (я только спросить), то ждать осталось {timeHour} часов и {timeMinuts} минут");
+    }
+}
